9861f1b2da39b9af0fb3a5ea39cc6d67fc0dae6b
Sebastian <500197548@fvtc.edu>
2024-09-25 18:34:50 -0700

    first commit

---
+++ DateNight/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace DateNight;
+using DateNight.Models;
+
+public partial class App : Application
+{
+    public static DateCalc dateCalc;
+    
+    public App()
+    {
+        InitializeComponent();
+
+        dateCalc = new DateCalc();
+        MainPage = new AppShell();
+    }
+}
---
+++ DateNight/Views/MoneyPage.xaml.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateNight.Views;
+
+public partial class MoneyPage : ContentPage
+{
+    public MoneyPage()
+    {
+        InitializeComponent();
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        try { lblDisplay.Text = App.dateCalc.GetTotalCost(); }
+        catch (Exception ex)
+        {
+            lblDisplay.Text = "$0.00";
+            DisplayAlert("Error", ex.Message, "Ok");
+        }
+    }
+}
